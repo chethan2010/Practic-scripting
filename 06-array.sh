a06fdcfe26396c2c9ec9ef6b94db968248e6189c
Hemanth <hemanthkumarsaketi@gmail.com>
2024-06-07 15:35:35 +0530

    first script

--- 06-array.sh
+++ 06-array.sh
@@ -2,6 +2,6 @@
 
 Movies= ("RRR" "Bahubali" "Rangastalam")
 
-echo "My fav movie 1st fav movie is:${Movies:0}"
-echo "My fav movie 2nd fav movie is:${Movies:1}"
-echo "My fav movie 3rd fav movie is:${Movies:2}"
+echo "My fav movie 1st fav movie is:${Movies:[0]}"
+echo "My fav movie 2nd fav movie is:${Movies:[1]}"
+echo "My fav movie 3rd fav movie is:${Movies:[2]}"
