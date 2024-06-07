6647fe73ba15c93f58ea32071ffcca3fa58e7c39
Hemanth <hemanthkumarsaketi@gmail.com>
2024-06-07 07:12:44 +0530

    first script

--- 03-Varablesuser.sh
+++ 03-Varablesuser.sh
@@ -2,7 +2,7 @@
 Person1:"Kalyan"
 Person2:"Anil"
 
-echo "$Person1:Hello $Person2 How are you?"
-echo "$Person2:Hi $Person1, Iam fine How are you?"
-echo "$Person1:Yeah iam fine $Person2, how is Your project going?"
-echo "$Person2:$Person1 Iam working on Project itis almost done.
+echo "$Person1::Hello $Person2 How are you?"
+echo "$Person2::Hi $Person1, Iam fine How are you?"
+echo "$Person1::Yeah iam fine $Person2, how is Your project going?"
+echo "$Person2::$Person1 Iam working on Project itis almost done.
