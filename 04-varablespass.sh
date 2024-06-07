0c6b7556a2d58e27f3f17286b6b66cff46296e10
Hemanth <hemanthkumarsaketi@gmail.com>
2024-06-07 14:51:13 +0530

    first script

--- 04-varablespass.sh
+++ 04-varablespass.sh
@@ -2,7 +2,7 @@
 $No1
 $No2
 
-echo "$No1::Hello $No2 How are you?"
-echo "$No2::Hi $No1, Iam fine How are you?"
-echo "$No1::Yeah iam fine $No2, how is Your project going?"
-echo "$No2::Iam working on Project it is almost done."
+echo "$No1:Hello $No2 How are you?"
+echo "$No2:Hi $No1, Iam fine How are you?"
+echo "$No1:Yeah iam fine $No2, how is Your project going?"
+echo "$No2:Iam working on Project it is almost done."
