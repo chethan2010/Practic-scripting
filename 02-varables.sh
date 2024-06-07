e2d2199948e88bef9aec724a93829eaeaf94f500
Hemanth <hemanthkumarsaketi@gmail.com>
2024-06-07 06:59:01 +0530

    first script

---
+++ 02-varables.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Kranthi:Hello, Hemanth How are you?"
+echo "Hemanth:Hi Kranthi, Iam fine How are you?"
+echo "Kranthi:Yeah iam fine Hemanth, how is Your project going?"
+echo "Hemanth:Kranthi Iam working on Project itis almost done"
