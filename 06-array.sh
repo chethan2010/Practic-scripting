9486aafe4b51d39466c42c0175b4a6ec13489a2c
Hemanth <hemanthkumarsaketi@gmail.com>
2024-06-07 15:48:58 +0530

    first script

--- 06-array.sh
+++ 06-array.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-Movie=("RRR" "Bahu" "Ranga")
+Movie=("RRR" "Dhoni" "Kgf")
 
 echo "My fav movie is: ${Movie[0]}"
-echo "My fav movie is: ${Movies[1]}"
-echo "My fav movie is: ${Movies[2]}"
+echo "My fav movie is: ${Movie[1]}"
+echo "My fav movie is: ${Movie[2]}"
