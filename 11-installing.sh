5bbff121e52e305ddadb66107ff7e55f1ab216ba
Hemanth <hemanthkumarsaketi@gmail.com>
2024-06-08 07:46:32 +0530

    installing

--- 11-installing.sh
+++ 11-installing.sh
@@ -5,6 +5,19 @@ TimeStamp=$(date +%F-%H-%M-%S)
 Script_Name=$(echo $0 | cut -d '.' -f1 )
 Log_file=/tmp/$TimeStamp-$Script_Name.log
 
+
+validate(){
+  
+if [ $1 -ne 0 ]
+
+    then
+        echo "$2...success"
+    else
+        echo "$2...Failure"
+    fi
+}
+
+
 if [ $UserID -ne 0 ]
 
 then
