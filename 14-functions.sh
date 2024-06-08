221c47bf4fee19181b91d311a376bc3fcf76c9c6
Hemanth <hemanthkumarsaketi@gmail.com>
2024-06-08 15:57:23 +0530

    installing

--- 14-functions.sh
+++ 14-functions.sh
@@ -16,3 +16,17 @@ else
 
 fi
 }
+
+if [ $USERID -ne 0 ]
+then
+    echo "Please run this script with root access."
+    exit 1 # manually exit if error comes.
+else
+    echo "You are super user."
+fi
+
+dnf install mysql -y &>>$LOGFILE
+VALIDATE $? "Installing MySQL"
+
+dnf install git -y &>>$LOGFILE
+VALIDATE $? "Installing Git"
