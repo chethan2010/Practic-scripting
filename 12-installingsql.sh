5a196296a0ff77d102b5c05d84270edb24390e0c
Hemanth <hemanthkumarsaketi@gmail.com>
2024-06-08 08:07:01 +0530

    installing

--- 12-installingsql.sh
+++ 12-installingsql.sh
@@ -1,7 +1,39 @@
 #!/bin/bash
 
 UserID=$(id -u)
-TimeStamp=$(date +%F-%H-%M-%S)
+#TimeStamp=$(date +%F-%H-%M-%S)
+#Script_Name=$(echo $0| cut -d '.' -f1)
+# echo "$UserID"
+# echo "$TimeStamp"
 
-echo "$UserID"
-echo "$TimeStamp"
+if [ $UserID -ne 0 ]
+then
+    echo "Please run Script as Superuser"
+    exit 1
+else
+    echo "You are Super user"
+    exit 1
+fi
+
+#dnf install mysql-y
+
+if [$? -ne 0]
+then
+    echo "Installtion of mysql is ..Failure"
+    exit 1
+else
+    echo "Insrallation mysql is success"
+    exit
+fi
+
+#dnf install git -y
+if [$? -ne 0]
+then 
+    echo "Installation of Git is Failed"
+    exit 1
+
+else 
+
+    echo "Installation of Git is Success"
+    exit 1
+fi
