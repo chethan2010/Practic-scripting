00ea7e9e5c0c876d71e420dc013631e1a5a1f976
Hemanth <hemanthkumarsaketi@gmail.com>
2024-06-08 07:01:04 +0530

    first script

--- 10-installing.sh
+++ 10-installing.sh
@@ -5,16 +5,16 @@ TimeStamp=$(date +%F-%H-%M-%S)
 Script_Name=$(echo $0| cut -d "." -f1)
 Logfile=/tmp/$TimeStamp-$Script_Name.log
 
-validate(){
-if [ $1 -ne 0 ]
+# validate(){
+# if [ $1 -ne 0 ]
 
-then 
-   echo " $2.. Failure"
-   exit 1
-else
-   echo "$2" Success
-   exit 1
-   }
+# then 
+#    echo " $2.. Failure"
+#    exit 1
+# else
+#    echo "$2" Success
+#    exit 1
+#    }
 
 if [ $UserID -ne 0 ]
 
