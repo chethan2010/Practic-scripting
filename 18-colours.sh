7afb0c41fdc7528b95e324ee88177a324d51cd02
Hemanth <hemanthkumarsaketi@gmail.com>
2024-06-09 07:49:37 +0530

    colours

--- 18-colours.sh
+++ 18-colours.sh
@@ -5,9 +5,10 @@ Time_Stamp=$(date +%F-%H-%M-%S)
 Script_Name=$(echo $0 | cut -d '.' -f) 
 logfile=$Time_Stamp-$SCRIPT_NAME.log
 
-if [ $USERID -ne 0 ]
+if [ $UserID -ne 0 ]
 then
     echo "You are not super user Please enter Script as Super user"
+    exit 1
 else
     echo "You are super User"
 fi
