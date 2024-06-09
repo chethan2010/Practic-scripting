2430a65121a54a70f570322be4812826ab117904
Hemanth <hemanthkumarsaketi@gmail.com>
2024-06-09 16:03:45 +0530

    colours

--- 18-colours.sh
+++ 18-colours.sh
@@ -2,8 +2,22 @@
 
 UserID=$(id -u)
 Time_Stamp=$(date +%F-%H-%M-%S)
-Script_Name=$(echo $0 | cut -d '.' -f) 
+Script_Name=$(echo $0 | cut -d '.' -f1) 
 logfile=$Time_Stamp-$SCRIPT_NAME.log
+
+R="e\[e31m"
+G="e\[e32m"
+N="e\[e0m"
+
+VALIDATE(){
+if [ $1 -ne o ]
+then    
+    echo "$2...$R failure $N"
+    exit 1
+else
+    echo "S2...$G Success $N"
+fi
+}
 
 if [ $UserID -ne 0 ]
 then
@@ -12,4 +26,9 @@ then
 else
     echo "You are super User"
 fi
-   
+
+dnf install msysql -y
+VALIDATE $? "Installing mysql"&>>$logfile
+
+dnf install git -y
+VALIDATE $? "installing git"&>>$logfile   
