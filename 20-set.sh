d38d0d7f4737d50a4df834333ac48190f752efd2
Hemanth <hemanthkumarsaketi@gmail.com>
2024-06-10 15:29:45 +0530

    colours

---
+++ 20-set.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+
+then 
+    echo "Please Execute this Script as Super user"
+
+else
+    echo "You are super user"
+
+fi
+
+dnf install mysqll -y
+
+dnf install giti =y
+
+echo "Script is Proceding"
