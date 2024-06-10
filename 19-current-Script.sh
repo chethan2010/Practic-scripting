f4c6e9d2df7eb98f1886d2222b605f9c2521c47d
Hemanth <hemanthkumarsaketi@gmail.com>
2024-06-10 14:18:47 +0530

    colours

--- 19-current-Script.sh
+++ 19-current-Script.sh
@@ -5,6 +5,5 @@ Course="Devops From Current script"
 echo "befor calling other script, course:$Course"
 echo "Process id for current shell script:$$"
 
-# source ./16-colours.sh
-
-# echo "After calling other script, course:$Course"
+source ./16-colours.sh
+echo "After calling other script, course:$Course"
