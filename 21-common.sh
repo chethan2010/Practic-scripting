a2610214c3897b9ca479502f4cb75f32374a6fcd
Hemanth <hemanthkumarsaketi@gmail.com>
2024-06-11 12:42:15 +0530

    set

--- 21-common.sh
+++ 21-common.sh
@@ -17,10 +17,10 @@ N="\e[0m"
 VALIDATE(){
 if [ $1 -ne 0 ]
 then 
-    echo "$2...$R Failure $N"
+    echo -e "$2...$R Failure $N"
     exit 1
 else
-    echo "$2...$G SUCCESS $N"
+    echo -e "$2...$G SUCCESS $N"
 fi
 }
 check_root(){
