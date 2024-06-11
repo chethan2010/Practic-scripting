773fa59002e2d1e51a340380bede13634407aea9
Hemanth <hemanthkumarsaketi@gmail.com>
2024-06-11 12:49:00 +0530

    set

--- 21-common.sh
+++ 21-common.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-handle_error(){
-echo "error occured at  line number: $1,error command:$2"
-}
-trap 'handle_error ${LINENO}"$BASH_COMMAND"' ERR
+# handle_error(){
+# echo "error occured at  line number: $1,error command:$2"
+# }
+# trap 'handle_error ${LINENO}"$BASH_COMMAND"' ERR
 
 USERID=$(id -u)
 date_Stamp=$(date +%F-%H-%M-%S)
